5a1a1ac57ab8c4b404ff583ea694cc60542b5166
Glitch (dzshpee) <none>
2019-08-04 07:29:11 +0000

    🛰💆 Updated with Glitch

--- commands/lockdown.js
+++ commands/lockdown.js
@@ -1,15 +1,15 @@
-const ms = require('ms');
+const ms = require("ms");
 exports.run = (client, message, args) => {
   if (!client.lockit) client.lockit = [];
-  const time = args.join(' ');
-  const validUnlocks = ['release', 'unlock'];
-  if (!time) return message.reply('You must set a duration for the lockdown in either hours, minutes or seconds');
+  const time = args.join(" ");
+  const validUnlocks = ["release", "unlock"];
+  if (!time) return message.reply("You must set a duration for the lockdown in either hours, minutes or seconds");
 
   if (validUnlocks.includes(time)) {
     message.channel.overwritePermissions(message.guild.id, {
       SEND_MESSAGES: null
     }).then(() => {
-      message.channel.send('Lockdown lifted.');
+      message.channel.send("Lockdown lifted.");
       clearTimeout(client.lockit[message.channel.id]);
       delete client.lockit[message.channel.id];
     }).catch(error => {
@@ -24,7 +24,7 @@ exports.run = (client, message, args) => {
         client.lockit[message.channel.id] = setTimeout(() => {
           message.channel.overwritePermissions(message.guild.id, {
             SEND_MESSAGES: null
-          }).then(message.channel.send('Lockdown lifted.')).catch(console.error);
+          }).then(message.channel.send("Lockdown lifted.")).catch(console.error);
           delete client.lockit[message.channel.id];
         }, ms(time));
 
@@ -37,13 +37,13 @@ exports.run = (client, message, args) => {
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['ld'],
+  aliases: ["ld"],
   permLevel: 2
 };
 
 exports.help = {
-  name: 'lockdown',
-  category: 'Moderation',
-  description: 'This will lock a channel down for the set duration, be it in hours, minutes or seconds.',
-  usage: 'Lockdown: lockdown [duration]\nRemove lockdown: lockdown release OR lockdown unlock'
+  name: "lockdown",
+  category: "Moderation",
+  description: "This will lock a channel down for the set duration, be it in hours, minutes or seconds.",
+  usage: "Lockdown: lockdown [duration]\nRemove lockdown: lockdown release OR lockdown unlock"
 };
